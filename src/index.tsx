cf0abdae188abebcf212a10d94ec43cc897ea261
Petr Pamuzhak <petromars444@gmail.com>
2023-02-26 13:24:16 +0300

    add react router v6

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import {createRoot, Root} from 'react-dom/client';
 import {App} from "./App";
+import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <div>
+                <h1>Hello World</h1>
+                <Link to="about">About Us</Link>
+            </div>
+        ),
+    },
+    {
+        path: "about",
+        element: <div>About</div>,
+    },
+]);
 
 const container = document.getElementById('root');
 // @ts-ignore
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <RouterProvider router={router} />
+);
